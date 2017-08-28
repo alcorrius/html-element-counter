83eab3dec7b2f341ad560e1450c470be072cb947
alcorrius <dotsenko.vitaliy@gmail.com>
2017-08-29 01:27:29 +0300

    update database models

--- app/models/Base.php
+++ app/models/Base.php
@@ -13,13 +13,40 @@ abstract class Base
 {
     protected $table = null;
 
+    private function checkItem($name)
+	{
+		$db = Database::DB();
+		$s = $db->prepare("SELECT * FROM {$this->table} WHERE name= :name");
+		$s->bindValue(':name', strval($name), \PDO::PARAM_STR);
+		$s->execute();
+		$row = $s->fetch();
+
+		return $row;
+	}
+
     public function getItem($id = null) {
         $db = Database::DB();
-        $sql = "SELECT * from {$this->table} where id= :id)";
+        $sql = "SELECT * from {$this->table} where id= :id";
         $s = $db->prepare($sql);
         $s->bindValue(':id', intval($id), \PDO::PARAM_INT);
         $result = $s->fetch();
 
         return $result;
     }
+
+	public function addItem($item)
+	{
+		$checkItem = $this->checkItem($item);
+		if($checkItem){
+			return $checkItem['id'];
+		}
+
+		$db = Database::DB();
+		$sql = "INSERT INTO {$this->table}(id, name) VALUES (null, :name)";
+		$s = $db->prepare($sql);
+		$s->bindValue(':name', strval($item), \PDO::PARAM_STR);
+		$result = $s->execute();
+
+		return $result == true ? $db->lastInsertId() : null;
+	}
 }
--- app/models/Request.php
+++ app/models/Request.php
@@ -8,8 +8,91 @@
 
 namespace models;
 
+use core\Database;
+
 
 class Request extends Base
 {
-    protected $table = 'request';
+	protected $table = 'request';
+
+	public function addItem($domain_id, $url_id = null, $element_id = null, $count=null, $time = null, $duration = null)
+	{
+		$db = Database::DB();
+		$sql = "INSERT INTO {$this->table}(id, domain_id, url_id, element_id, count, time, duration) VALUES (null, :domain_id, :url_id, :element_id, :count, :time, :duration)";
+		$s = $db->prepare($sql);
+		$s->bindValue(':domain_id', intval($domain_id), \PDO::PARAM_INT);
+		$s->bindValue(':url_id', intval($url_id), \PDO::PARAM_STR);
+		$s->bindValue(':element_id', intval($element_id), \PDO::PARAM_INT);
+		$s->bindValue(':count', intval($count), \PDO::PARAM_INT);
+		$s->bindValue(':time', $time);
+		$s->bindValue(':duration', strval($duration), \PDO::PARAM_STR);
+		$result = $s->execute();
+
+		return $result;
+	}
+
+	public function getLastRequest($url_id, $element_id)
+	{
+		$db = Database::DB();
+		$sql = "SELECT request.*, url.name as url_name, element.name as element_name from {$this->table} join url on url.id = request.url_id join element on element.id = request.element_id where url_id= :url_id and element_id= :element_id ORDER BY id DESC LIMIT 1";
+		$s = $db->prepare($sql);
+		$s->bindValue(':url_id', intval($url_id), \PDO::PARAM_INT);
+		$s->bindValue(':element_id', intval($element_id), \PDO::PARAM_INT);
+		$s->execute();
+		$result = $s->fetch();
+
+		return $result;
+	}
+
+	public function getTotalUrlByDomainId($domain_id)
+	{
+		$db = Database::DB();
+		$sql = "SELECT count(distinct url_id) as count from {$this->table} where domain_id= :domain_id";
+		$s = $db->prepare($sql);
+		$s->bindValue(':domain_id', intval($domain_id), \PDO::PARAM_INT);
+		$s->execute();
+		$result = $s->fetch();
+
+		return $result;
+	}
+
+	public function getAvgFetchTimeByDomainId24h($domain_id)
+	{
+		$time = new \DateTime();
+		$time->sub(new \DateInterval('P1D'));
+		$db = Database::DB();
+		$sql = "SELECT avg(duration) as avg from {$this->table} where domain_id= :domain_id and time > :time";
+		$s = $db->prepare($sql);
+		$s->bindValue(':domain_id', intval($domain_id), \PDO::PARAM_INT);
+		$s->bindValue(':time', $time->format('Y-m-d H:i'));
+		$s->execute();
+		$result = $s->fetch();
+
+		return $result;
+	}
+
+	public function getTotalElementByDomainId($domain_id, $element_id)
+	{
+		$db = Database::DB();
+		$sql = "SELECT sum(count) as sum from (select * from {$this->table} where domain_id= :domain_id and element_id= :element_id group by url_id, element_id) as t1";
+		$s = $db->prepare($sql);
+		$s->bindValue(':domain_id', intval($domain_id), \PDO::PARAM_INT);
+		$s->bindValue(':element_id', intval($element_id), \PDO::PARAM_INT);
+		$s->execute();
+		$result = $s->fetch();
+
+		return $result;
+	}
+
+	public function getTotalElement($element_id)
+	{
+		$db = Database::DB();
+		$sql = "SELECT sum(count) as sum from (select * from {$this->table} where element_id= :element_id group by url_id, element_id) as t1";
+		$s = $db->prepare($sql);
+		$s->bindValue(':element_id', intval($element_id), \PDO::PARAM_INT);
+		$s->execute();
+		$result = $s->fetch();
+
+		return $result;
+	}
 }
